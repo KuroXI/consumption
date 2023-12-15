c158d8a15ef4d356b1929a744523e878b26bc4f8
KuroXI <vgonzales5157@gmail.com>
2023-12-15 20:51:31 +0800

    [dev] feat/fix: update theme and fix feature title color

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -5,8 +5,12 @@ import { Button } from "../ui/button";
 import { GoogleIcon } from "../ui/icons";
 import Image from "next/image";
 import Cover from "@/assets/cover.svg";
+import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
 
 export const Hero = () => {
+  const { theme, setTheme } = useTheme();
+
   return (
     <div className="flex h-screen w-screen items-center justify-center gap-64 px-24">
       <div className="flex flex-col gap-10">
@@ -17,10 +21,21 @@ export const Hero = () => {
           </p>
         </div>
         <div className="flex flex-col gap-2">
-          <Button className="flex w-fit gap-2 px-7" onClick={() => signIn("google")}>
-            <GoogleIcon className="h-5 w-5" />
-            Join with Google
-          </Button>
+          <div className="flex gap-2">
+            <Button className="flex gap-2 px-7" onClick={() => signIn("google")}>
+              <GoogleIcon className="h-5 w-5" />
+              Join with Google
+            </Button>
+            <Button
+              size="icon"
+              variant="outline"
+              className="flex gap-2"
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            >
+              {theme === "dark" ? <Sun /> : <Moon />}
+            </Button>
+          </div>
+
           <p className="text-sm text-muted-foreground">
             By joining, you agree to our{" "}
             <span className="text-primary underline">Terms & Conditions</span>.
