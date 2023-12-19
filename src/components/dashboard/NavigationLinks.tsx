1ad30dae22af0b3823d59de8cfb7cb560d71d388
KuroXI <vgonzales5157@gmail.com>
2023-12-19 23:15:37 +0800

    [dev] feat: add expense form

---
+++ src/components/dashboard/NavigationLinks.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  { href: "/dashboard", label: "Overview" },
+  { href: "/dashboard/expenses", label: "Expenses" },
+  { href: "/dashboard/settings", label: "Settings" },
+];
+
+export const NavigationLinks = () => {
+  const pathname = usePathname();
+
+  return links.map(({ href, label }) => (
+    <Link
+      key={href}
+      href={href}
+      className={cn(
+        "transition-colors hover:text-foreground/80",
+        pathname === href ? "text-foreground" : "text-foreground/60",
+      )}
+    >
+      {label}
+    </Link>
+  ));
+};
--- src/components/dashboard/UserProfile.tsx
+++ src/components/dashboard/UserProfile.tsx
@@ -1,15 +1,59 @@
-import { type Session } from "next-auth";
+"use client";
+
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { useTheme } from "next-themes";
+import { Switch } from "../ui/switch";
+import { useSession } from "next-auth/react";
+import { Skeleton } from "../ui/skeleton";
 
-type UserProfileProps = {
-  session: Session;
-};
+export const UserProfile = () => {
+  const { data: session } = useSession();
+  const { theme, setTheme } = useTheme();
 
-export const UserProfile = ({ session }: Readonly<UserProfileProps>) => {
-  return (
-    <Avatar>
-      <AvatarImage src={session.user.image!} alt={session.user.id} />
-      <AvatarFallback>{session.user.name?.charAt(0).toUpperCase()}</AvatarFallback>
-    </Avatar>
+  return session ? (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar>
+          <AvatarImage src={session.user.image!} alt={session.user.id} />
+          <AvatarFallback>{session.user.name?.charAt(0).toUpperCase()}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel>
+          <div className="flex flex-col gap-1">
+            <p className="text-sm font-medium">{session.user.name}</p>
+            <p className="text-xs text-muted-foreground">{session.user.email}</p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem
+            className="flex items-center justify-between"
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          >
+            Dark Mode
+            <Switch
+              checked={theme === "dark"}
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            />
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Log out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  ) : (
+    <Skeleton className="h-8 w-8 rounded-full" />
   );
 };
