724e46049759de1da44bdc1593ba58041abbacf9
KuroXI <vgonzales5157@gmail.com>
2024-02-05 16:48:24 +0800

    [dev] fix: remove un-used components and packages

--- src/components/dashboard/expenses/ExpenseForm.tsx
+++ src/components/dashboard/expenses/ExpenseForm.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
 import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { cn } from "@/lib/utils";
 import { api } from "@/trpc/react";
-import { CalendarIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
@@ -99,38 +96,6 @@ export const ExpenseForm = ({ className }: ExpenseFormProps) => {
             </FormItem>
           )}
         />
-        {/* <FormField
-          control={form.control}
-          name="date"
-          render={({ field }) => (
-            <FormItem className="flex flex-col gap-1">
-              <FormLabel>Date</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button
-                    variant={"outline"}
-                    className={cn(
-                      "justify-start text-left font-normal",
-                      !field.value && "text-muted-foreground",
-                    )}
-                  >
-                    <CalendarIcon className="mr-2 h-4 w-4" />
-                    {field.value ? field.value.toDateString() : <span>Pick a date</span>}
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    initialFocus
-                    mode="single"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
-                  />
-                </PopoverContent>
-              </Popover>
-            </FormItem>
-          )}
-        /> */}
         <Button className="w-full" type="submit">
           Submit
         </Button>
