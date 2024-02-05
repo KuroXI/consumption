cd29fe018d8321320fc2e434911473fe49eef6c9
KuroXI <vgonzales5157@gmail.com>
2024-02-05 16:45:42 +0800

    [dev] fix: linting problem

--- src/app/dashboard/transactions/page.tsx
+++ src/app/dashboard/transactions/page.tsx
@@ -5,6 +5,7 @@ import { authOptions } from "@/server/auth";
 import { redirect } from "next/navigation";
 import { Footer } from "@/components/dashboard/Footer";
 import { TransactionsClient } from "@/components/dashboard/transactions/TransactionsClient";
+import { api } from "@/trpc/server";
 
 export const metadata = {
   title: "Transactions - Consumption",
@@ -16,12 +17,14 @@ export default async function Page() {
   const session = await getServerSession(authOptions);
   if (!session) return redirect("/");
 
+  const transactions = await api.transaction.all.query();
+
   return (
     <>
       <NavigationBar />
       <MaxWidthWrapper className="min-h-screen py-5">
         <h1 className="px-5 pb-3 text-lg font-semibold">Transactions</h1>
-        <TransactionsClient />
+        <TransactionsClient transactions={transactions} />
       </MaxWidthWrapper>
       <Footer />
     </>
